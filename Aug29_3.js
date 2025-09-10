4400638b82e807d04b4e7e93b8c887e95674f8f9
Adesh0509 <143960607+Adesh0509@users.noreply.github.com>
2025-09-10 20:16:02 +0530

    Add files via upload

---
+++ Aug29_3.js
@@ -0,0 +1,50 @@
+import express from "express"
+import multer from "multer"
+import path from "path"
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.set("views", "C:/Users/adesh/OneDrive/Desktop/Nodenew/views");
+
+
+app.use(express.json()); 
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  res.render("profilepic")
+})
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads")
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname)
+  },
+})
+
+const upload= multer({ storage: storage })
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.body)
+  console.log(req.file)
+  res.redirect("/")
+})
+
+app.get ("/uploads/:filename", (req, res) => {
+    const filename = req.params.filename;
+    const location=Path2D.join('./Day9/uploads', filename);
+    res.download(location, filename, (err) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log("File downloaded successfully");
+        }
+    })
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
